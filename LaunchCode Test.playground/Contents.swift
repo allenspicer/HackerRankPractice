060c32d3a0bdfbd59929d42d9b6e556fa4ac07ae
Allen Spicer <allenspicer@gmail.com>
2016-08-11 16:30:03 -0400

    Test

---
+++ LaunchCode Test.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//first is count
+//for all following find the one only there once
+
+
+func lonelyInteger(arr: [Int]) -> Int {
+    var count = arr[0]
+    while count > 0 {
+        let index = 
+        
+        
+        count = count - 1
+    }
+    
+    return Int
+}
