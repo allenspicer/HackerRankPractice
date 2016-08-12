b2c28ef718e33d1c3902afd34365e7d57d1283ca
Allen Spicer <allenspicer@gmail.com>
2016-08-11 21:06:46 -0400

    Finished

--- LaunchCode Test.playground/Contents.swift
+++ LaunchCode Test.playground/Contents.swift
@@ -9,49 +9,45 @@ import UIKit
 
 func lonelyInteger(arr: [Int]) -> Int {
     
+    
     //create mutable copy
     var mutableArr = arr
     
     //remove count from copy
     mutableArr.removeAtIndex(0)
     
-    print(mutableArr)
-    
     //create result variable
     var result = 0
-
+    
     //create counter for loop
     var i = 1
     
     //loop through all the remaining numbers
     while i < arr[0] {
-
-//check whether the number at this index is equal to any others
-
+        
+        //check whether the number at this index is equal to any others
+        
         //for all of the relevant numbers
         for number in mutableArr{
-           
+            
             //if this number is equal to any of the others remove it from the copy
             if arr[i] == number{
-                mutableArr.removeAtIndex(i)
-                print(mutableArr)
-
+                mutableArr.removeAtIndex(0)
+                
             }
         }
-
-    i = 1 + i
         
+        i = 1 + i
     }
-    if mutableArr.count != 0{
-    print(mutableArr)
-
     result = mutableArr[0] as Int
-    }
     
     return result
 }
 
+
 lonelyInteger([3,1,1,2])
+lonelyInteger([1,1])
+
 
 
 
