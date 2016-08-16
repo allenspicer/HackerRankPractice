7ffe21b1ca329d198f25c7bb8b79e9316b5a8a83
Allen Spicer <allenspicer@gmail.com>
2016-08-15 21:36:32 -0400

    Update

--- ClosestNumberPractice.playground/Contents.swift
+++ ClosestNumberPractice.playground/Contents.swift
@@ -6,6 +6,8 @@ let N = 10
 
 var A = [-20, -3916237,-357920,-3620601,7374819,-7330761,30,6246457,-6461594,266854]
 
+var result = Array<Int>()
+
 
 A.sortInPlace()
 
@@ -14,15 +16,16 @@ var greatestDiff = 0
 
 while i < A.count {
     let prior = i - 1
-    if (A[prior]-A[i] > greatestDiff){
+    let currentDiff = A[prior]-A[i]
+    if (currentDiff > greatestDiff){
         //record which two numbers made the diff
-        
+        result =
         
         //set B as the difference
         
     }
     if (A[prior]-A[i] == greatestDiff){
-        //record which two numbers made the same diff
+        //append the two numbers that made the same diff
         
         
         
