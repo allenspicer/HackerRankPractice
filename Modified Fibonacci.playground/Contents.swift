7ceb06e417bb19951e6b8ba423097f02796d66c8
Allen Spicer <allenspicer@gmail.com>
2016-08-17 09:21:34 -0400

    Update

--- Modified Fibonacci.playground/Contents.swift
+++ Modified Fibonacci.playground/Contents.swift
@@ -5,18 +5,30 @@ import UIKit
 
 var input = [0,1,5]
 
+//save and remove N
 let N = input.last
-
 input.removeLast()
 
-print(input)
-
+//create count variable
 var i = 0
 
+//create holder for results
 var result = input
 
 while i < N{
+    let next = i + 1
+    if next <= input.last{
+        let one = input[i]
+        let two = input[next]
+        let new = one + (two*two)
+        result.append(new)
+
+    }
+    
+    
     
     
     i = i + 1
 }
+
+print(result.last!)
