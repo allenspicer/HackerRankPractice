3531567f91e9b9464c313617cafa767b178049a1
Allen Spicer <allenspicer@gmail.com>
2016-08-17 13:33:27 -0400

    Update

--- FriPractice.playground/Contents.swift
+++ FriPractice.playground/Contents.swift
@@ -9,6 +9,12 @@ import UIKit
 
 //invert a string in place
 
+//Build a program that will solve simple math problems of X digits by only using single digit operations.
+
+// Given a series of parentheses determine if they are balanced or not.
+
+// Solve for n factorial using recursion.
+
 
 //determine if these parantheses are valid
 
@@ -16,8 +22,23 @@ import UIKit
 //recursive fibonacci sequence
 
 
+//method that determines number of handshakes when given number of people
 
 
+//method that takes the sum of any number of numbers
+
+
+//describe MVC and how it is implemented in iOS
+
+//discuss autolayout example
+
+//discuss view lifecycle
+
+//discuss memory
+
+//buy-sell stock problem
+
+//palindrome
 
 
 
