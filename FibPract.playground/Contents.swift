9f8f138359ca2a15dc758492f3bb63070a680803
Allen Spicer <allenspicer@gmail.com>
2016-08-24 11:43:54 -0400

    Update

---
+++ FibPract.playground/Contents.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+//fib: 1 1 2 3 5 8 13
+//idx: 0 1 2 3 4 5 6
+
+func fibGenerator (idx:Int) -> Int{
+    
+    var currentValue = 1
+    var fibArray = [1,1]
+    var i = 0
+    while i < idx{
+        
+        if let previousValue = fibArray[i]{
+            
+            if let twoBackValue = fibArray[i-1]{
+                currentValue = previousValue + twoBackValue
+            }
+        }
+        
+        fibArray.append(currentValue)
+        i = i + 1
+    }
+    
+    let result = fibArray[idx]
+    
+    return result
+}
+
+[1,1,2]
+[1,1,2,3]
